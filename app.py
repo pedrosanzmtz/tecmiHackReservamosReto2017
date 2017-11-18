0c5f2c0ad6f1e211ab6802e84244528ecb5dcc94
pedrosanzmtz <pedrosanzmtz@gmail.com>
2017-11-18 17:10:38 -0600

    flask app created

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/places", methods=['POST'])
+def places():
+	print(request.data)
+	return request.data
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
