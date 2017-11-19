c29c2a13a15b2be52d0e6ff0d98dcc7f58a62b62
pedrosanzmtz <pedrosanzmtz@gmail.com>
2017-11-18 18:08:15 -0600

    added quotes route

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-import os
-from flask import Flask, request
+import os, json, reservamosApi
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-@app.route("/places", methods=['POST'])
+@app.route('/places', methods=['POST'])
 def places():
-	print(request.data)
-	return request.data
+	data = json.loads(request.json)	
+	response_dict = reservamosApi.getPlaces(data['city'])
+	response_json = json.dumps(response_dict)
+	response = Response(response=response_json, status=200, mimetype="application/json")
+	return response
+
+@app.route('/quotes', methods=['POST'])
+def quotes():
+	data = json.loads(request.json)
+	response_dict = reservamosApi.getQuotes(data)
+	response_json = json.dumps(response_dict)
+	response = Response(response=response_json, status=200, mimetype="application/json")
+	return response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
